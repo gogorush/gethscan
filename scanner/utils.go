4eeaf001d0dd272d96eb6cc48f5bade8e2b319f5
weijun-sh <huangweijun0322@163.com>
2021-12-14 19:07:38 +0800

    add GetData

--- scanner/utils.go
+++ scanner/utils.go
@@ -7,13 +7,13 @@ import (
 // GetData get data[start:start+size] (won't out of index range),
 // and right padding the bytes to size long
 func GetData(data []byte, start, size uint64) []byte {
-        length := uint64(len(data))
-        if start > length {
-                start = length
-        }
-        end := start + size
-        if end > length {
-                end = length
-        }
-        return common.RightPadBytes(data[start:end], int(size))
+	length := uint64(len(data))
+	if start > length {
+		start = length
+	}
+	end := start + size
+	if end > length {
+		end = length
+	}
+	return common.RightPadBytes(data[start:end], int(size))
 }
