add43854d947d9e98574fde8efcaf1a7dcb50517
weijun-sh <huangweijun0322@163.com>
2022-02-11 12:45:39 +0800

    check swap post result: wrong contract

--- scanner/scanswap.go
+++ scanner/scanswap.go
@@ -104,6 +104,7 @@ const (
 	errDepositLogNotFountorRemoved = "return error: json-rpc error -32099, verify swap failed! deposit log not found or removed"
 	swapIsClosedResult             = "swap is closed"
 	swapTradeNotSupport            = "swap trade not support"
+	txWithWrongContract            = "tx with wrong contract"
 )
 
 var startHeightArgument int64
@@ -733,6 +734,10 @@ func checkRouterStatus(status string, args interface{}) error {
 	if strings.Contains(status, routerSwapExistResult) ||
 		strings.Contains(status, routerSwapExistResultTmp) {
 		log.Info("post router swap already exist", "swap", args)
+		return nil
+	}
+	if strings.Contains(status, txWithWrongContract) {
+		log.Info("post router swap failed, tx with wrong contract", "swap", args)
 		return nil
 	}
 	err := errors.New(status)
